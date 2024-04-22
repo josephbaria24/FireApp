42c0a51ca03e9e68321e96d6a4e378b393b22cac
Joseph <202180024@psu.palawan.edu.ph>
2024-04-22 20:11:14 +0800

    successful chart setup

--- projectsite/projectsite/urls.py
+++ projectsite/projectsite/urls.py
@@ -7,5 +7,5 @@ from fire import views
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('', HomePageView.as_view(), name='home'),
-
+    path('dashboard_chart', ChartView.as_view(), name='dashboard-chart'),
 ]
