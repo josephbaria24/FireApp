dfe7e08232e4b7325b626729b13a6362a62b2902
Joseph <202180024@psu.palawan.edu.ph>
2024-04-22 18:06:19 +0800

    successful chart setup

--- projectsite/projectsite/urls.py
+++ projectsite/projectsite/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.urls import path
 
-from fire.views import HomePageView
+from fire.views import HomePageView, ChartView
 from fire import views
 
 urlpatterns = [
